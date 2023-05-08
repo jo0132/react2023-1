77b0af3985274f9f5bb400f7186434fa19ea9426
jo0132 <noji70770@gmail.com>
2023-05-08 13:04:36 +0900

    2023.05.08

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';   // imr이 단축어
+// import ReactDOM from 'react-dom';   // imrd이 단축어
+
+// function App(){
+//   return (
+//     <div>
+//       <h1>리액트</h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>리액트</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
